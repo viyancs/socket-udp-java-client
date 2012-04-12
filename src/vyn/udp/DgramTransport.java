c17285bba4664d0e4e1609828dd879972f72bb52
indoallcode <msofyancs@yahoo.com>
2012-04-12 19:17:47 +0700

    adding comment

--- src/vyn/udp/DgramTransport.java
+++ src/vyn/udp/DgramTransport.java
@@ -5,9 +5,16 @@
 package vyn.udp;
 
 /**
- *
- * @author mohamadsofiyani
- */
+* UDP Implementation - socket-udp-java-client
+*
+* interface for DatagramSocket
+*
+* @author M Sofiyan
+* @email msofyancs@gmail.com
+* @skypeid viyancs
+* if you want to using part of full this code, please don't remove this comment
+*
+**/
 public interface DgramTransport {
     void connect();
     void close();
